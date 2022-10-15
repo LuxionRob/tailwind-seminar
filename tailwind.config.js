c4fabee700654e6825ef0756f9988b34a1405c0f
LuxioRob <aidaynhi8@gmail.com>
2022-10-15 23:16:13 +0700

    Add feature configuration ex

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,34 @@
 module.exports = {
   content: ['./src/index.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      // => @media (min-width: 480px) { ... }
+      md: '790px',
+      // => @media (min-width: 790px) { ... }
+      lg: '970px',
+      // => @media (min-width: 970px) { ... }
+      xl: '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+
+    fontFamily: {
+      serif: ['PT serif', 'serif'],
+      sans: ['Roboto', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      colors: {
+        'imperial-red': '#e63946',
+        honeydew: '#f1faee',
+        'powder-blue': '#a8dadc',
+        'celadon-blue': '#457b9d',
+        'prussian-blue': '#1d3557',
+      },
+    },
   },
   plugins: [],
 }
